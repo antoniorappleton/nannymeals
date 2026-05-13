7498d36278684d3fa0ffa01f48e8d001a82ce189
António Appleton <antonioappleton@gmail.com>
2026-05-13 19:10:26 +0100

    nannymeals| nova versão CludeJoão

--- app/public/sw.js
+++ app/public/sw.js
@@ -1,4 +1,4 @@
-// Service Worker Neutralizado para v32 - Swipe UX Update
+// Service Worker Neutralizado para v33 - Swipe UX Update & Logic Fixes
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => {
   event.waitUntil(
